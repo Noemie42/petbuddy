2b035cf9dacb39e6745f309e583d22a205bf449c
Noemie77 <noemie.darcillon@gmail.com>
2021-05-20 09:46:24 +0200

    ajout html dur pages footer

--- public/temp/faq.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'inc/header.inc.php';
-include 'inc/nav.inc.php';
-?>
-  <div class="content" style="height: 700px; margin: 20px auto">
-   
-  </div>
-<?php
-include 'inc/footer.inc.php';
